4a28e538fa2f36b7afaf753bcbaeed197715120d
Enny <99821597+Enreinha@users.noreply.github.com>
2023-03-15 16:09:35 +0100

    radio imported

--- security_project.py
+++ security_project.py
@@ -2,6 +2,9 @@ from microbit import *
 import utime
 import machine
 import music
+import radio
+
+radio.config(group = 13)
 
 def get_distance():
     # Distanz messen
@@ -15,17 +18,16 @@ def get_distance():
 start_distance = get_distance()
 print("Starting up, initial dist: ", start_distance)
 while True:
+    message_in = radio.receive()
     current_distance = get_distance()
     if abs(start_distance - current_distance) > 10:
         sleep(3000)
         print(current_distance)
         second_distance = get_distance()
         if abs(start_distance - second_distance) > 10:
-            print("Intrusion detected:", second_distance)
-            music.pitch(2000, wait=False)
-            while not button_a.get_presses():
-                sleep(59)
-            music.stop()
-            sleep(1000)
+            radio.send("Intrusion detected")
+  
+    if message_in == "Reset":
+        start_distance = get_distance()
     
 

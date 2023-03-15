364412c9c69a34f594cf71782275f4f26b258eb3
kimngo01 <kimngo01@ksr.ch>
2023-03-15 16:47:18 +0100

    Update microbit_receiver.py

--- microbit_receiver.py
+++ microbit_receiver.py
@@ -3,15 +3,23 @@ import music
 import radio
 
 radio.config(group = 13)
+radio.on()
 
+receive = 1
 while True:
     message_in = radio.receive()
-    if message_in == "Intrusion detected":
-        display.scroll(message_in)
+
+    if message_in == "Intrusion detected" and receive==1:
+        display.scroll(message_in, wait=False)
         music.pitch(2000, wait = False)
         while not button_a.get_presses():
-            sleep(60)
-            music.stop()
-            sleep(1000)
+            sleep(50)
+        music.stop()
+        receive = 0
+        message_in = "no"
+        display.clear()
+
     if button_b.get_presses():
         radio.send("Reset")
+        receive = 1
+
